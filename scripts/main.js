17f1be09135c1222c4565570cd44ed73307dc5f1
iliko12345678 <gvazavailiko@gmail.com>
2025-04-29 18:08:54 +0200

    Update 3.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,17 @@
-let plus_sign = document.querySelector(".icon")
-let plus_div_function = document.querySelector(".plus_div_function")
-let top_icon = document.querySelectorAll(".top_icon")
-let inbox_img = document.querySelector(".inbox_img")
-let help_img = document.querySelector(".help_img")
-let top_input = document.querySelector(".top_input")
+document.addEventListener("DOMContentLoaded", () => {
+  const plusSign = document.querySelector(".plus_div img");
+  const plusDivFunction = document.querySelector(".plus_div_function");
+  const topInput = document.querySelector(".top_input");
 
-console.log("dsdfsdsff")
+  plusSign.addEventListener("click", () => {
+    if (plusDivFunction.style.display === "none" || plusDivFunction.style.display === "") {
+      plusDivFunction.style.display = "flex";
+    } else {
+      plusDivFunction.style.display = "none";
+    }
+  });
 
-plus_sign.addEventListener("click", function(){
-    console.log("dfijnn")
-    if (plus_div_function.style.display === "none"){
-        plus_div_function.style.display = "block"
-    }
-    else{
-        plus_div_function.style.display = "none"
-    }
-})
+  topInput.addEventListener("input", () => {
+    console.log("Searching: " + topInput.value);
+  });
+});
